28b8f547df702053c0fb0e643d7c00dacc11f8af
Frokesy <akindeleayanfeoluwa@gmail.com>
2022-09-07 08:40:01 +0100

    implemented weather data

--- src/components/AsyncCityView.vue
+++ src/components/AsyncCityView.vue
@@ -7,8 +7,8 @@
  
     <div>
         <h1>{{ route.params.city }}</h1>
-        <p class="" v-if="weatherData">
-          {{ new Date(weatherData.currentTime).toLocaleDateString(
+        <p class="">
+          {{ new Date(weatherData?.currentTime).toLocaleDateString(
               "en-US",
               {
                   weekday: "short",
@@ -18,7 +18,7 @@
           ) 
           }}
           {{ 
-          new Date(weatherData.currentTime).toLocaleTimeString(
+          new Date(weatherData?.currentTime).toLocaleTimeString(
               "en-US",
               {
                   hour: "2-digit",
@@ -27,7 +27,34 @@
           ) 
           }}
         </p>
-        <!-- <span>{{ weatherData.current.feels_like }}</span> -->
+        <span>{{ weatherData?.current.temp }}&deg;</span>
+        <p>Feels like {{ weatherData?.current.feels_like }}&deg;</p>
+        <p>{{ weatherData?.current.weather[0].description }}</p>
+        <img :src="`https://openweathermap.org/img/wn/${weatherData?.current.weather[0].icon}@2x.png`" alt="weather-icon">
+    </div>
+    <hr>
+    <div>
+        <h2>Hourly Weather</h2>
+        <div class="hourly">
+            <div v-for="hour in weatherData?.hourly" :key="hour.dt">
+                <p>{{ new Date(hour.dt * 1000).toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" }) }}</p>
+                <img :src="`https://openweathermap.org/img/wn/${hour?.weather[0].icon}@2x.png`" alt="weather-icon">
+                <p>{{ hour.temp }}&deg;</p>
+            </div>  
+        </div>
+    </div>
+
+    <hr>
+    <div>
+        <h2>Weekly Weather</h2>
+        <div class="weekly">
+            <div v-for="day in weatherData?.daily" :key="day.dt">
+                <p>{{ new Date(day.dt * 1000).toLocaleDateString("en-US", { weekday: "short" }) }}</p>
+                <img :src="`https://openweathermap.org/img/wn/${day?.weather[0]. icon}@2x.png`" alt="weather-icon" />
+                <p>H: {{ day.temp.max }}&deg;</p>
+                <p>L: {{ day.temp.min }}&deg;</p>
+            </div>
+        </div>   
     </div>
  </template>
 
@@ -41,13 +68,13 @@ export default defineComponent({
     data() {
         return{
             route: useRoute(),
-            weatherData: {} as any,
+            weatherData: null as any,
         }
     },
     async mounted () {
         const getWeatherData = async () => {
             try {
-                const weatherData = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${this.route.query.lat}&lon=${this.route.query.lng}&exclude={part}&appid=5fbdce717539e1f16120b7e52b90e251&units=imperial`)
+                const weatherData = await axios.get(`https://api.openweathermap.org/data/3.0/onecall?lat=${this.route.query.lat}&lon=${this.route.query.lng}&exclude={part}&appid=5fbdce717539e1f16120b7e52b90e251&units=metric`)
 
                 //calculate current date and time
                 const currentDate = new Date().getTimezoneOffset() * 60 * 1000;
@@ -72,5 +99,19 @@ export default defineComponent({
 </script>
 
 <style>
-
+  .hourly {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    max-width: screen;
+    width: 100%;
+    overflow-x: scroll;
+  } 
+  .weekly {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    max-width: screen;
+    width: 100%;
+  } 
 </style>
